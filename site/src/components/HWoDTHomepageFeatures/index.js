ebb0d03ed00aa2993324f074557345d67b25b920
Andrea Giulianelli <andrygiulio@gmail.com>
2025-01-14 14:58:17 +0100

    chore: set homepage

--- site/src/components/HWoDTHomepageFeatures/index.js
+++ site/src/components/HWoDTHomepageFeatures/index.js
@@ -4,32 +4,32 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Uniform Interface for DTs',
+    Svg: require('@site/static/img/uniform-interface.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        The HWoDT follows the <strong>REST</strong> architectural style defining a Uniform Interface
+        that accomodates the heterogeneity of existing Digital Twins technologies.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Focus on your application instead of DT technologies',
+    Svg: require('@site/static/img/application-focus.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Consumers (Applications) of DT services can navigate and use DT ecosystems regardless 
+        of the underlying technology by leveraging a <strong>hypermedia-based uniform interface</strong>.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Wide support',
+    Svg: require('@site/static/img/supported-technologies.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        HWoDT strategy is to add an interoperability layer on top of existing technologies.
+        The <strong>HWoDT Framework</strong> provides support for the main DT technologies.
       </>
     ),
   },
